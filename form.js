7cfcbef585fe46c8d20cda760c5a30ec48356878
anastasia <smolina.anastasia.14@gmail.com>
2021-02-28 00:44:40 +0300

    form

--- form.js
+++ form.js
@@ -1,5 +1,27 @@
-
-
+btn.onclick = function (event) {
+    event.preventDefault();
+    let user = {
+        userName: document.getElementById('name').value,
+        userLastName: document.getElementById('lastName').value,
+        email: document.getElementById('email').value,
+        password: document.getElementById('pass').value,
+        password2: document.getElementById('pass2').value,
+        sex: document.querySelector('.sex:checked').value,
+        birth: document.getElementById('birth').value
+    }
+    fetch("https://httpbin.org/post", {
+        method: 'POST',
+        body: JSON.stringify(user),
+        headers: {
+            'Content-Type': 'application/jsonn;charset=utf-8'
+        },
+    })
+    .then(response => response.json())
+    .then(user => {
+        console.log(user);
+    })
+    .catch(error => console.log(error));
+}
 
 /*
 
