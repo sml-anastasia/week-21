6bb7611d664ffb21b8a8630d51a90f09a7eece43
anastasia <smolina.anastasia.14@gmail.com>
2021-02-27 22:29:17 +0300

    giphy update

--- giphy.js
+++ giphy.js
@@ -1,16 +1,14 @@
 function searchGif(giphy) {
     giphy = document.getElementById('giphySearch').value;
     fetch("https://api.giphy.com/v1/gifs/search?api_key=ssCdjHoF9IkPIuo0ANcld5kooQnfiaw6&q=" + giphy + "&limit=5")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        document.getElementById('giphyResult').src = data.data[0].url;
-    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            let gifImg = '';
+            for (i = 0; i < data.data.length; i++) {
+                let gifSrc = data.data[i].images.preview_gif.url;
+                gifImg += `<img class="gif-img" src="${gifSrc}">`;
+            }
+            document.getElementById('giphyResult').innerHTML = gifImg;
+        })
 }
-
-//gif => document.getElementById('giphyResult').innerHTML = gif.data)
-/*
-Сделайте форму поиска по GIF-кам к открытому AP
-Используйте GET-запрос с параметрами в строке адреса. Страница должна выводить не более 5 картинок одновременно.
-
-*/
